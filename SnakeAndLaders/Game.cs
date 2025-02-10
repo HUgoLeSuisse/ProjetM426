173c98a5636d644d67a5dbc1016a5bedcbdcef3a
nelson.rss <nelson.rss@eduge.ch>
2025-02-10 15:13:03 +0100

    Tout de base

--- SnakeAndLaders/Game.cs
+++ SnakeAndLaders/Game.cs
@@ -5,12 +5,7 @@ namespace SnakeAndLaders
         public Game()
         {
             InitializeComponent();
-            button1.Text = "ASd";
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Hello");
-        }
     }
 }
